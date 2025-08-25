5eabda74c872d9bf1cc1a1b844a76ced5206212f
Vanessa Durán <104865481+Vanedrn03@users.noreply.github.com>
2025-08-25 16:50:44 -0600

    base de conocimientos

---
+++ base.pl
@@ -0,0 +1,79 @@
+aldea(rivoria).
+aldea(montelar).
+aldea(durania).
+
+vive(don_Alonso, montelar).
+vive(donia_Beatriz, montelar).
+vive(felipe, montelar).
+vive(catalina, montelar).
+vive(emilia, montelar).
+vive(hector, rivoria).
+vive(teresa, rivoria).
+vive(don_Esteban, rivoria).
+vive(donia_Mariela, rivoria).
+vive(rodolfo, rivoria).
+vive(ismael, rivoria).
+vive(clara, rivoria).
+vive(mateo, durania).
+vive(irene, durania).
+
+esposos(don_Alonso, donia_Beatriz).
+esposos(rodolfo, teresa).
+esposos(don_Esteban, donia_Mariela).
+esposos(catalina, andres).
+esposos(mateo, irene).
+
+amigos(ismael, hector).
+
+exesposos(andres, clara).
+
+hijos(donia_Mariela, clara).
+hijos(don_Esteban, clara).
+hijos(don_Esteban, rodolfo).
+hijos(donia_Mariela, rodolfo).
+hijos(don_Esteban, ismael).
+hijos(donia_Mariela, ismael).
+hijos(andres, sofia).
+hijos(clara, sofia).
+hijos(rodolfo, lucas).
+hijos(teresa, lucas).
+hijos(rodolfo, martina).
+hijos(teresa, martina).
+hijos(don_Alonso, felipe).
+hijos(donia_Beatriz, felipe).
+hijos(don_Alonso, catalina).
+hijos(donia_Beatriz, catalina).
+hijos(don_Alonso, emilia).
+hijos(donia_Beatriz, emilia).
+hijos(rodolfo, lucas).
+
+oficios(felipe, explorador_montanias).
+oficios(catalina, curandera).
+oficios(emilia, ceramista).
+oficios(andres, comerciante).
+oficios(hector, herrero).
+oficios(teresa, tejedoras).
+oficios(clara, maestra).
+oficios(mateo, agricultor).
+oficios(irene, ganadera).
+oficios(lucas, herrero).
+oficios(rodolfo, agricultor).
+
+vende(irene, mercado_Rivoria).
+
+alianza(rivoria, durania).
+division(rivoria, montelar).
+
+rivalidad(rodolfo, felipe).
+rivalidad(catalina, clara).
+
+fiestas(rivoria, fiesta_rio).
+fiestas(montelar, ceremonia_luna).
+fiestas(durania, feria_grano).
+
+jefe_aldea(don_Esteban, rivoria).
+jefe_aldea(don_Alonso, montelar).
+
+futuro(sofia, curandera).
+futuro(lucas, herreria).
+futuro(martina, ceramica).
